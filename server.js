f36010bbd7d8a4ec89df8c4c2e5e6405c17bcf34
Ari <ari.basil.khan@gmail.com>
2025-04-07 23:29:27 -0400

    Edited module.

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'pages')));
+app.use(cors());
 
-app.get("/:page", (req, res, next) => {
-  const page = req.params.page;
-  const filePath = path.join(__dirname, 'pages', `${page}.js`);
-  
+app.use(bodyParser.json());
+
+app.get('/:folder', (req, res, next) => {
+  const folder = req.params.folder;
+  const filePath = path.join(process.cwd(), 'pages', folder, 'index.js');
+
   res.sendFile(filePath, (err) => {
     if (err) {
       console.error(err);
@@ -22,14 +25,20 @@ app.get("/:page", (req, res, next) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'index.js'));
+app.use(express.static(path.join(process.cwd(), 'pages')));
+
+app.get('/index.html', (req, res) => {
+  res.redirect(301, '/');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'pages', 'index.js'));
 });
 
 app.use((req, res) => {
-  res.status(404).send("404 Not Found");
+  res.status(404).send('404 Not Found');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
